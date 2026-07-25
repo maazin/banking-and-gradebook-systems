255f169d55a2f3e58a67f1c37658a07bfab0e8f1
Maazin Shaikh <111378922+maazin@users.noreply.github.com>
2026-07-24 20:11:24 -0400

    Add enums for grading categories and letter grades

---
+++ assignment2_gradebook/gradebook/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Category(Enum):
+    HOMEWORK = "HOMEWORK"
+    QUIZZES = "QUIZZES"
+    MIDTERM = "MIDTERM"
+    FINAL_EXAM = "FINAL_EXAM"
+
+class LetterGrade(Enum):
+    A = "A" 
+    B = "B"
+    C = "C" 
+    D = "D"
+    F = "F" 
